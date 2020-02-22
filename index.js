800f819a3544c8e45f12cf4b8293b1b27f2513b5
Pedro <hegemcodes@gmail.com>
2020-02-22 21:16:24 +0100

    First steps and ping command.

--- index.js
+++ index.js
@@ -2,15 +2,42 @@ const { Client } = require("discord.js");
 const { config } = require("dotenv");
 
 const client = new Client({
-    disableEveryone: true
+  disableEveryone: true
 });
 
 config({
-    path: __dirname + "/.env"
+  path: __dirname + "/.env"
 });
 
 client.on("ready", () => {
-    console.log("I'm online!")
+  console.log(`I am online, my name is ${client.user.username}`);
+  client.user.setPresence({
+    status: "online",
+    game: {
+      name: "lolis breath.",
+      type: "LISTENING"
+    }
+  });
+});
+
+client.on("message", async message => {
+  const prefix = "..";
+
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd === "ping") {
+    const msg = await message.channel.send(`Pinging...`);
+    msg.edit(` Pong\nLatency is ${Math.floor(msg.createdAt - message.createdAt)}\nAPI Latency ${Math.round(client.ping)}ms`);
+  }
+
 });
 
 client.login(process.env.TOKEN);
