383e05794b47d849fdadb65e3668aab205a3b40a
Pedro <hegemcodes@gmail.com>
2020-02-22 20:28:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client } = require("discord.js");
+const { config } = require("dotenv");
+
+const client = new Client({
+    disableEveryone: true
+});
+
+config({
+    path: __dirname + "/.env"
+});
+
+client.on("ready", () => {
+    console.log("I'm online!")
+});
+
+client.login(process.env.TOKEN);
